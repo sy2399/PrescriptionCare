e19ad289abbfdd71fd1b1fe165d18ea55aecc0f2
ftatp <ftatp5901@gmail.com>
2017-09-22 13:14:33 +0900

    Addeda new view page for the dashboard

--- matchings/views.py
+++ matchings/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.views.generic.base import TemplateView
+
+from matchings.models import Disease, Prescription
 # Create your views here.
+
+class datashow(ListView):
+	template_name = 'datashow.html'
+
+	def get_queryset(self):
+		return Disease.objects.order_by('-STARTDATE')[:10]
 
 class m4876_00(TemplateView):
 	template_name = 'm4876.html'
