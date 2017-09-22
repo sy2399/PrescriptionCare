2b39ae9d98e3001974e64b400537f25d13717dd9
ftatp <ftatp5901@gmail.com>
2017-09-22 13:14:03 +0900

    Addeda new url page for the dashboard

--- matchings/urls.py
+++ matchings/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from matchings import views
 
 urlpatterns = [
+	url(r'^$', views.datashow.as_view(), name='datashow'),
 	url(r'^m4876_00/$', views.m4876_00.as_view(), name='m4876_00'),
 	url(r'^m4876_01/$', views.m4876_01.as_view(), name='m4876_01'),
 ]
