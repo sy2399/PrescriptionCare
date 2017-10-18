ee480f35568dcc388e36fe93747d34113935f9c5
ftatp <ftatp5901@gmail.com>
2017-10-18 21:52:28 +0900

    Made a page to compare 2 models

--- matchings/urls.py
+++ matchings/urls.py
@@ -4,6 +4,7 @@ from matchings import views
 urlpatterns = [
 	url(r'^$', views.datashow.as_view(), name='datashow'),
 	url(r'^match_disease/$', views.MatchFormView.as_view(), name='match_disease'),
+	url(r'^model_comparison/', views.MatchFormView, name='models_test'),
 	url(r'^m4876_00/$', views.m4876_00.as_view(), name='m4876_00'),
 	url(r'^m4876_01/$', views.m4876_01.as_view(), name='m4876_01'),
 ]
--- matchings/views.py
+++ matchings/views.py
@@ -36,6 +36,23 @@ class MatchFormView(FormView):
 
 		return render(self.request, self.template_name, context)
 
+class ModelCompareFormView(FormView):
+	form_class = MatchForm
+	template_name = 'models_test_page.html'
+
+	def form_valid(self, form):
+		schWord = '%s' % self.request.POST['match_word']
+		#prescription_list = Prescription_List.objects.filter(Q(ORDERCODE__icontains=schWord)).distinct()
+
+		model = NetworkxModel()
+		context = {}
+		context['form'] = form
+		context['search_term'] = schWord
+
+		context['disease_list'] = model.get_disease_by_networkx(dxcode_input=schWord)
+
+		return render(self.request, self.template_name, context)
+
 
 class m4876_00(TemplateView):
 	template_name = 'm4876.html'
