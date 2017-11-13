6d36986417ae00155055b71b40a62c20affeb8ac
imsoyeong <ter194@ajou.ac.kr>
2017-11-13 15:59:03 +0900

    상병과연관된처방 html/css수정

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -35,6 +35,7 @@ $(function(){
 	    mouseenter : function(){
 	        var tr = $(this);
             var td = tr.children();//선택한 상병값
+            td.eq(0).css("background-color", "#abd9e9");
             var pre_td = td.eq(1);
             //여기서 ajax로 관련처방리스트 불러서 아래의 pre_td.text()안에 넣으면댐
             pre_td.text(td.text())
@@ -44,7 +45,7 @@ $(function(){
 	        var tr = $(this);
             var td = tr.children();//선택한 상병값
             var pre_td = td.eq(1);
-
+            td.eq(0).css("background-color", "#ffffff");
             pre_td.text("")
 	    }
 
