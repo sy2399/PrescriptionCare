f7ce5d5cfbe8126f69f9002944d1b6b74a1beea4
Kyungsik Han <zenithhan@gmail.com>
2017-12-14 09:13:55 +0900

    심사과화면수정
    - 기존에 선택된 내용이 있으면 표시해주는 작업 완료

--- matchings/views.py
+++ matchings/views.py
@@ -13,7 +13,7 @@ from django.template import RequestContext
 from django.db.models import Q
 
 from django.contrib.auth.models import User
-from matchings.models import Disease, Disease_name, Prescription, Review, Notice, Doctor_diagnose 
+from matchings.models import Disease, Disease_name, Prescription, Review, Notice, Doctor_diagnose
 from matchings.forms import MatchForm
 
 #from matchings.neural_net_model import NeuralNetwork
@@ -52,15 +52,15 @@ def match_disease(request):
 	if request.method == "POST":
 		print(request.POST)
 
-		inputPreCode = request.POST.get("inputPreCode").split(" ")[1]
+		inputPreCode = request.POST.get("inputPreCode")#.split(" ")[1]
 		print(inputPreCode)
 		inputPreName = Prescription.objects.get(Q(ordercode=inputPreCode)).ordername
-		noticeArea = request.POST.get("noticeArea")
+		noticeArea = request.POST.get("noticeArea").strip()
 		if request.POST.get("flag") == "option1":
 			flag = False
 		else:
 			flag = True
-		
+
 		if Notice.objects.filter(Q(ordercode=inputPreCode)).count() == 1:
 			notice = Notice.objects.get(Q(ordercode=inputPreCode))
 			notice.notice_description = noticeArea
@@ -75,19 +75,26 @@ def match_disease(request):
 					)
 			notice.save()
 
-
+		##########################################
+		# 수정 by khan
+		##########################################
+		# 해당 처방에 대한 기존 상병은 모두 삭제함
+		Review.objects.filter(Q(ordercode=inputPreCode)).delete()
+		# 새롭게 업데이트된 선택 결과를 삽입
 		for checked_dxcode in request.POST.getlist("checked_discode"):
-			if Review.objects.filter(Q(ordercode=inputPreCode) & Q(dxcode=checked_dxcode)).count() == 1:
-				review = Review.objects.get(Q(ordercode=inputPreCode) & Q(dxcode=checked_dxcode))
-				review.frequency += 1
-				review.save()
-			else:
-				review = Review(
-							ordercode=inputPreCode,
-							dxcode=checked_dxcode,
-							#dxcode_name
-						)
-				review.save()
+			review = Review(
+				ordercode=inputPreCode,
+				dxcode=checked_dxcode,
+				# dxcode_name
+			)
+			review.save()
+			'''
+			if Review.objects.filter(Q(ordercode=inputPreCode) & Q(dxcode=checked_dxcode)).count() == 1: # 존재하는 경우
+				#review = Review.objects.get(Q(ordercode=inputPreCode) & Q(dxcode=checked_dxcode))
+				#review.frequency += 1
+				#review.save()
+			else: # 없는 경우 새로 기록
+			'''
 
 
 	return render(request, 'disease_search.html', context)
@@ -109,10 +116,10 @@ def search_prescription(request):
 
 def search_disease(request):
 	if request.method == "POST":
-		search_list = request.POST['search_list']
+		search_list = request.POST['search_list'].strip()
 	else:
 		search_list = ''
-	
+
 	context = {}
 	context['search_term'] = search_list
 	#idx = prescriptiondf["ordercode"][prescriptiondf["ordercode"].split(" ")[0]==search_list].index()
@@ -120,8 +127,50 @@ def search_disease(request):
 
 	# context['search_term'] = Prescription.objects.get()
 
+
+	######################################################
+	#  Added by khan Dec. 14 2017
+	######################################################
+	print("search_list: ", search_list)
 	disease_list = NXmodel.get_disease(ordercode_input=search_list, num=10)
-	context['NX_disease_list'] = disease_list
+	# context['NX_disease_list'] = disease_list
+
+	# 해당 처방에 대한 Review 정보 받아오기
+	selected_list = []
+	if Review.objects.filter(Q(ordercode=search_list)).count() != 0: # 정보가 있으면 (복수 가능)
+		reviews = Review.objects.filter(Q(ordercode=search_list))
+		print(reviews)
+
+		# 이미 선택된 상병인지 확인을 위한 코드
+		for disease in disease_list:
+			selected = 0
+			for review in reviews:
+				if disease[0] == review.dxcode:
+					selected = 1
+					break
+			if selected == 1:
+				selected_list.append([disease, 1])
+			else:
+				selected_list.append([disease, 0])
+	else:
+		print("해당처방 리뷰 정보 없음")
+
+	context['NX_disease_list'] = selected_list
+
+	######################################################
+	# 해당 처방에 대한 Notice 정보 받아오기
+	if Notice.objects.filter(Q(ordercode=search_list)).count() == 1: # 정보가 있으면 (단일만 가능)
+		notice = Notice.objects.get(Q(ordercode=search_list))
+		print(notice.notice_description)
+		context['notice'] = notice
+	else:
+		print("해당처방 Notice 정보 없음")
+		context['notice'] = ''
+
+	######################################################
+	# End by khan
+	######################################################
+
 
 #	disease_name_list = []
 #
@@ -129,7 +178,7 @@ def search_disease(request):
 #
 #	for i in np.arange(len(disease_list)):
 #		connection[i] = NXmodel.find_dxcode(disease_list[i][0])[:10]
-#	
+#
 #	context['connection'] = connection
 
 #	print(search_list)
@@ -142,10 +191,10 @@ def search_disease(request):
 class ModelCompareFormView(FormView):
 	form_class = MatchForm
 	template_name = 'models_test_page.html'
-	
-#	NNmodel = NeuralNetwork()	
+
+#	NNmodel = NeuralNetwork()
 #	NXmodel = NetworkxModel()
-	
+
 	def form_valid(self, form):
 		schWord = '%s' % self.request.POST['match_word']
 		#prescription_list = Prescription_List.objects.filter(Q(ORDERCODE__icontains=schWord)).distinct()
@@ -192,7 +241,7 @@ class UserService(FormView):
 			#print(Review.objects.filter(ordercode=code).count())
 			if Review.objects.filter(ordercode=code).count() == 0:
 				continue
-		
+
 			hosp_prescription = Review.objects.filter(ordercode=code)
 			for item in hosp_prescription:
 				hosp_prescriptions.append(item)
@@ -203,7 +252,7 @@ class UserService(FormView):
 			if Prescription.objects.filter(ordercode=code).count() != 1:
 				continue
 			sys_prescription = Prescription.objects.get(ordercode=code)
-			
+
 			if Notice.objects.filter(ordercode=code).count() == 1:
 				notice = Notice.objects.get(ordercode=code)
 
@@ -217,7 +266,7 @@ class UserService(FormView):
 				continue
 
 			sys_prescriptions.append(sys_prescription)
-		
+
 
 			networkx_disease_list = NXmodel.get_disease(ordercode_input=schWord, num=3)
 			networkx_disease_lists.append(networkx_disease_list)
@@ -227,7 +276,7 @@ class UserService(FormView):
 
 		context["sys_prescriptions"] = zip(sys_prescriptions, networkx_disease_lists)
 		#context['networkx_disease_lists'] = networkx_disease_lists
-		
+
 		#context["prescription_list"] = zip(hosp_prescriptions, notices)
 
 		context["search_prescription_list"] = zip(np.arange(1, 1 + len(search_prescription_list)).tolist(), search_prescription_list)
