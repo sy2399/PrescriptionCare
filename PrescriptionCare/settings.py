75e215669858f7d888f66537a200d11ca6df150f
ftatp <ftatp5901@gmail.com>
2018-01-15 15:14:14 +0900

    Redirectec login action to match disease

--- PrescriptionCare/settings.py
+++ PrescriptionCare/settings.py
@@ -31,7 +31,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #LOGIN_URL
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/matchings/match_disease/'
 
 # Application definition
 
